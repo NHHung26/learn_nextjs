6eb16e999e7f67bed531720f221cd1c8e40fb58a
Huy Hung <laptopzus@Huy-Hungs-MacBook-Pro.local>
2023-05-12 23:37:43 +0700

    frist commit

---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export interface AboutPage {}
+
+export default function AboutPage(props: AboutPage) {
+  return (
+    <div>
+      <h1>About Page</h1>
+    </div>
+  );
+}
